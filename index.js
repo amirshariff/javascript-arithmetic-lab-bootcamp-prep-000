bf894b3db80f07957e52a635f7ea43fa11e5df65
amir shariff <amirshariff@msn.com>
2019-07-15 20:20:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a, b) {
+  return a + b
+}
+
+add(5, 6);
+
+function subtract(a, b){
+  return a - b
+}
+
+subtract(5, 6);
+
+function multiply(a, b){
+  return a * b
+}
+
+multiply(5, 6);
+
+function divide(a, b){
+  return a / b
+}
+
+divide(5, 6);
+
+
+function inc (a){
+  return a+1
+}
+
+inc(a)
+
+function dec (a){
+  return a-1
+}
+
+dec(a)
+
+function makeInt(hello){
+  return parseInt(hello)
+}
+
+makeInt(a)
+
